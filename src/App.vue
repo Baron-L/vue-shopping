2584da69119c3105a99f03cc94f89fccd1dd491b
Leo <Baron_Ll@163.com>
2021-12-29 18:27:12 +0800

    静态页面组件拆分

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    我是App组件
+    <Header></Header>
+    <!-- 路由组件出口地方 -->
+    <router-view></router-view>
+    <Footer v-show="$route.meta.isShow"></Footer>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
-
+  components: {
+    Header,
+    Footer
+  },
 }
 </script>
 
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <type-nav></type-nav>
+      <list-container></list-container>
+      <Recommend> </Recommend>
+      <Rank></Rank>
+      <Like></Like>
+      <Floor></Floor>
+      <Floor></Floor>
+      <Brand></Brand>
+  </div>
+</template>
+
+<script>
+import ListContainer from './ListContainer'
+import Recommend from './Recommend'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
+export default {
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  },
+}
+</script>
+
+<style>
+
+</style>
