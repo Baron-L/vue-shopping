494695814592aed5bd4ffd25f5844e93a53e58eb
Leo <Baron_Ll@163.com>
2021-12-30 11:02:38 +0800

    封装axios及请求进度条插件

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+import requests from "./request"
+
+// 三级联动接口
+
+// 发请求:axios返回结果为Promise对象
+export const reqgetCategoryList = () => requests({url : '/product/getBaseCategoryList', method: 'get'})
+
