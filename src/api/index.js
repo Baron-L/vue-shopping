c0691f27273ea17dd86bfbc1b4db01d9688638e3
Leo <Baron_Ll@163.com>
2022-01-05 17:13:09 +0800

    搜索模块静态页面

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,18 @@ import requests from "./request"
 
 import mockRequests from './mockAjax'
 // 发请求:axios返回结果为Promise对象
-export const reqCategoryList = () => requests({url : '/product/getBaseCategoryList', method: 'get'})
+export const reqCategoryList = () => requests.get(`/product/getBaseCategoryList`)
 
+// 获取seach模块数据 /api/list   POST   参数
+export const reqGetSearchInfo = (params) => requests({
+    url: '/list',
+    method: 'post',
+    data: params
+})
 
 // 获取banner接口  mock模拟数据
 export const reqGetBannerList = () => mockRequests.get('/banner')
 // 获取floor接口  mock模拟数据 
 export const reqGetFloorList = () => mockRequests.get('/floor')
+
+
--- src/store/search/index.js
+++ src/store/search/index.js
@@ -1,12 +1,27 @@
+import { reqGetSearchInfo } from "@/api";
 // Search模块的小仓库
-const state = {}
-const mutations = {}
-const actions = {}
-const getters = {}
+const state = {
+  searchList: {},
+};
+const mutations = {
+  GETSEARCHLIST(state, searchList) {
+    state.searchList = searchList;
+  },
+};
+const actions = {
+  // 第一个参数：上下文参数， 第二个参数载荷
+  async getSearchList({ commit }, params = {}) {
+    let result = await reqGetSearchInfo(params);
+    if (result.code === 200) {
+      commit("GETSEARCHLIST", result.data);
+    }
+  },
+};
+const getters = {};
 
 export default {
-    state,
-    mutations,
-    actions,
-    getters
-}
+  state,
+  mutations,
+  actions,
+  getters,
+};
