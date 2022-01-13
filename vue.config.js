704ad7aab8659bfb4066c77ea093690beb3e0478
Leo <Baron_Ll@163.com>
2022-01-13 15:49:30 +0800

    打包忽略map文件

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-    // 关闭ESlint
-    lintOnSave: false,
-    //配置代理跨域
-    devServer: {
-        proxy: {
-            "/api": {
-                target: "http://39.98.123.211",
-            },
-        },
+  // 打包时不带map文件
+  productionSourceMap: false,
+  // 关闭ESlint
+  lintOnSave: false,
+  //配置代理跨域
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://39.98.123.211",
+      },
     },
-}
+  },
+};
